494ff4a9cb0f46a411f9c36c8b625b5f6fb888d0
febinstephen <godsonfebin@gmail.com>
2017-12-21 17:55:49 +0530

    modified settings

--- facebook/settings.py
+++ facebook/settings.py
@@ -129,10 +129,10 @@ ACCOUNT_ADAPTER = 'utils.adapters.MessageFreeAdapter'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
+    os.path.join(BASE_DIR, "static")
 ]
 
 
